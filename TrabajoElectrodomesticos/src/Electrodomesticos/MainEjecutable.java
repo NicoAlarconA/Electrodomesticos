f0bd940aaa1eee0fd649d3fab2495545f0b49a73
nicolasrobertoalarconalallana <nicolasrobertoalarconalallana@localhost>
2020-02-21 19:17:58 -0300

    Creacion Ejecutable

---
+++ TrabajoElectrodomesticos/src/Electrodomesticos/MainEjecutable.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Electrodomesticos;
+
+/**
+ * @author nicolasrobertoalarconalallana
+ *
+ */
+public class MainEjecutable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
